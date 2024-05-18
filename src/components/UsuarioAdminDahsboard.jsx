f5301d9bb0bbebf4862a71ca40ba0d96e2085f8d
Cualquier nombre <rodrigogalvez2706@gmail.com>
2024-05-18 16:24:30 -0500

    avance usuario_admin_dashboard

--- src/components/UsuarioAdminDahsboard.jsx
+++ src/components/UsuarioAdminDahsboard.jsx
@@ -4,9 +4,20 @@ function UsuarioAdminDashboard() {
     return (
         <>
         <Cabecera/>
-        <div>
-            <h1>Esta es la pagina de inicio</h1>
-        </div>
+        <header>Cuenta - Usuario Administrador - Dashboard</header>
+        <nav class="sidebar">
+            <ul>
+                <li>Admin</li>
+                <li>Dashboard</li>
+            </ul>
+        </nav>
+        <main class="dashboard">
+            <div class="card">68<br/>Órdenes del día de hoy</div>
+            <div class="card">12<br/>Usuarios nuevos</div>
+            <div class="card income">S/ 13,500.00<br/>Ingresos de hoy</div>
+        </main>
+        <footer>LA TIENDITA DEL ABUELO © 2010 - 2020 Privacy — Terms</footer>
+
         <Pie/>
         </>
     )
