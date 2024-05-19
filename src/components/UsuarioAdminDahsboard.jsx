f94b2353f8bec026f29e7f94242ab4633e600536
Cualquier nombre <rodrigogalvez2706@gmail.com>
2024-05-19 10:35:37 -0500

    admin dashboard css

--- src/components/UsuarioAdminDahsboard.jsx
+++ src/components/UsuarioAdminDahsboard.jsx
@@ -7,20 +7,38 @@ function UsuarioAdminDashboard() {
     return (
         <>
         <Cabecera/>
-        <header>Cuenta - Usuario Administrador - Dashboard</header>
-        <nav class="sidebar">
-        <ul>
-            <li>Admin</li>
-            <li>Dashboard</li>
-  
-        </ul>
-        </nav>
-        <main class="dashboard">
-            <div class="card">68<br/>Órdenes del día de hoy</div>
-            <div class="card">12<br/>Usuarios nuevos</div>
-            <div class="card income">S/ 13,500.00<br/>Ingresos de hoy</div>
-
-        </main>
+        <div class="container">
+        <div class="sidebar">
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Usuarios registrados</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Órdenes</a></li>
+                <li><a href="#">Productos más vendidos</a></li>
+                <li><a href="#">Series</a></li>
+            </ul>
+        </div>
+        <div class="main-content">
+            <div class="header">
+                <h2>Dashboard</h2>
+                <a href="#">Cambiar Fecha o Periodo</a>
+            </div>
+            <div class="dashboard-cards">
+                <div class="card">
+                    <h1>68</h1>
+                    <p>Órdenes del día de hoy</p>
+                </div>
+                <div class="card">
+                    <h1>12</h1>
+                    <p>Usuarios nuevos</p>
+                </div>
+                <div class="card">
+                    <h1>S/13,500.00</h1>
+                    <p>Ingresos de hoy</p>
+                </div>
+            </div>
+        </div>
+    </div>
         
 
         <Pie/>
