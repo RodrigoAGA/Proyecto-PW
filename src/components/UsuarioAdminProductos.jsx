70ce9de99561f03cf83beeaeb7d265d549d4a328
Cualquier nombre <rodrigogalvez2706@gmail.com>
2024-05-19 10:57:43 -0500

    usuario admin productos css

--- src/components/UsuarioAdminProductos.jsx
+++ src/components/UsuarioAdminProductos.jsx
@@ -7,22 +7,94 @@ function UsuarioAdminProductos() {
     return (
         <>
         <Cabecera/>
-        <header>Cuenta - Usuario Administrador - Dashboard</header>
-        <nav class="sidebar">
-        <ul>
-            <li>Admin</li>
-            <li>Dashboard</li>
-  
-        </ul>
-        </nav>
-        <main class="dashboard">
-            <div class="card">68<br/>Órdenes del día de hoy</div>
-            <div class="card">12<br/>Usuarios nuevos</div>
-            <div class="card income">S/ 13,500.00<br/>Ingresos de hoy</div>
-
-        </main>
-        
-
+        <div class="container">
+            <div class="sidebar">
+                <ul>
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#">Usuarios registrados</a></li>
+                    <li><a href="#">Productos</a></li>
+                    <li><a href="#">Órdenes</a></li>
+                    <li><a href="#">Productos más vendidos</a></li>
+                    <li><a href="#">Series</a></li>
+                </ul>
+            </div>
+            <div class="main-content">
+                <div class="header">
+                    <h2>Productos</h2>
+                    <a href="#" class="button">+ Agregar Producto</a>
+                </div>
+                <div class="search-bar">
+                    <input type="text" placeholder="Buscar por Id, serie o detalle..."></input>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Detalle</th>
+                            <th>Serie</th>
+                            <th>Precio</th>
+                            <th>Fecha de Registro</th>
+                            <th>Stock</th>
+                            <th>Estado</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Manga Dragon Ball Vol 1</td>
+                            <td>Dragon Ball</td>
+                            <td>S/ 30.99</td>
+                            <td>11/02/2022</td>
+                            <td>12</td>
+                            <td>Activo</td>
+                            <td><a href="#">Ver</a> | <a href="#">Desactivar</a></td>
+                        </tr>
+                        <tr>
+                            <td>10</td>
+                            <td>Manga Sailor Moon Vol 9</td>
+                            <td>Sailor Moon</td>
+                            <td>S/ 30.99</td>
+                            <td>11/02/2022</td>
+                            <td>10</td>
+                            <td>Activo</td>
+                            <td><a href="#">Ver</a> | <a href="#">Desactivar</a></td>
+                        </tr>
+                        <tr>
+                            <td>11</td>
+                            <td>Lost BluRay Complete</td>
+                            <td></td>
+                            <td>S/ 30.99</td>
+                            <td>11/02/2022</td>
+                            <td>100</td>
+                            <td>Activo</td>
+                            <td><a href="#">Ver</a> | <a href="#">Desactivar</a></td>
+                        </tr>
+                        <tr>
+                            <td>14</td>
+                            <td>Manga Dragon Ball Vol 2</td>
+                            <td>Dragon Ball</td>
+                            <td>S/ 30.99</td>
+                            <td>11/02/2022</td>
+                            <td>12</td>
+                            <td>Activo</td>
+                            <td><a href="#">Ver</a> | <a href="#">Desactivar</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="pagination">
+                    <a href="#">Anterior</a>
+                    <a href="#" class="active">1</a>
+                    <a href="#">2</a>
+                    <a href="#">3</a>
+                    <a href="#">4</a>
+                    <a href="#">5</a>
+                    <a href="#">...</a>
+                    <a href="#">11</a>
+                    <a href="#">Siguiente</a>
+                </div>
+            </div>
+        </div>
         <Pie/>
         </>
     )
