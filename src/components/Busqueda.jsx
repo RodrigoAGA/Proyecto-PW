7b6b960fac23f98a691c36adae901ce05b10edd3
daniel Arenas <20180114@aloe.ulima.edu.pe>
2024-07-07 00:27:37 -0500

    busqueda-detalle

--- src/components/Busqueda.jsx
+++ src/components/Busqueda.jsx
@@ -1,126 +1,101 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Pie from './Pie';
 import Cabecera from './Cabecera';
 import './Parte1.css';
 
 function Busqueda() {
+    const [productosData, setProductosData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [sortCriteria, setSortCriteria] = useState('Ninguno');
+    const [sortDirection, setSortDirection] = useState('asc');
+    const productsPerPage = 6;
+
     useEffect(() => {
-        const productosData = [
-            { 
-                id: 1,
-                imagenSrc: 'ruta/de/imagen1.jpg', 
-                titulo: 'Voltron Mini Action Voltron Vehicle Force Figure (Standard)',
-                fabricante: 'Bandai',
-            },
-            { 
-                id: 2,
-                imagenSrc: 'ruta/de/imagen2.jpg', 
-                titulo: 'Saint Seiya HQS Plus Athena Limited Edition Statue',
-                fabricante: 'Bandai - Serie: Saint Seiya HQS',
-            },
-            { 
-                id: 3,
-                imagenSrc: 'ruta/de/imagen3.jpg', 
-                titulo: 'Saint Seiya Premium Masterline Dragon Shiryu Final Bronze Cloth 1/4 Scale Statue',
-                fabricante: 'Prime 1 Studio - Serie: Knights of The Zodiac',
-            },
-            { 
-                id: 4,
-                imagenSrc: 'ruta/de/imagen4.jpg', 
-                titulo: 'Voltron Mini Action Voltron Vehicle Force Figure (Standard)',
-                fabricante: 'Por: Bandai',
-            },
-            { 
-                id: 5,
-                imagenSrc: 'ruta/de/imagen5.jpg', 
-                titulo: 'Voltron Mini Action Voltron Vehicle Force Figure (Standard)',
-                fabricante: 'Por: Bandai',
-            },
-            { 
-                id: 6,
-                imagenSrc: 'ruta/de/imagen6.jpg', 
-                titulo: 'Saint Seiya HQS Plus Athena Limited Edition Statue',
-                fabricante: 'Bandai - Serie: Saint Seiya HQS',
-            },
-        ];
-
-        const productosContainer = document.getElementById('productos');
-        const paginacionContainer = document.getElementById('paginacion');
-
-        const generarProductos = () => {
-            productosData.forEach((producto) => {
-                const containerDiv = document.createElement('div');
-                containerDiv.classList.add('productos-container2');
-
-                const imagenElement = document.createElement('img');
-                imagenElement.src = producto.imagenSrc;
-                imagenElement.alt = '';
-                containerDiv.appendChild(imagenElement);
-
-                const productoDetalleDiv = document.createElement('div');
-                productoDetalleDiv.classList.add('producto-detalle');
-                const tituloP = document.createElement('p');
-                tituloP.classList.add('producto-titulo');
-                tituloP.textContent = producto.titulo;
-                const fabricanteP = document.createElement('p');
-                fabricanteP.classList.add('producto-fabri');
-                fabricanteP.textContent = `Por: ${producto.fabricante}`;
-                const precioP = document.createElement('p');
-                precioP.classList.add('producto-precio');
-                const precioAleatorio = (Math.random() * (5000 - 100) + 100).toFixed(2);
-                precioP.textContent = `S/ ${precioAleatorio}`;
-                productoDetalleDiv.appendChild(tituloP);
-                productoDetalleDiv.appendChild(fabricanteP);
-                productoDetalleDiv.appendChild(precioP);
-
-                containerDiv.appendChild(productoDetalleDiv);
-
-                containerDiv.addEventListener('click', () => {
-                    const url = `./DetalleProducto.html?id=${producto.id}&titulo=${encodeURIComponent(producto.titulo)}&fabricante=${encodeURIComponent(producto.fabricante)}&imagenSrc=${encodeURIComponent(producto.imagenSrc)}&precio=${encodeURIComponent(precioAleatorio)}`;
-                    window.location.href = url;
-                });
-
-                productosContainer.appendChild(containerDiv);
-            });
-        };
-
-        const cambiarPagina = (nuevaPagina) => {
-            window.location.href = `?pagina=${nuevaPagina}`;
-        };
-
-        if (paginacionContainer) {
-            const anterior = paginacionContainer.querySelector('.anterior');
-            const siguiente = paginacionContainer.querySelector('.siguiente');
-            const paginas = paginacionContainer.querySelectorAll('.pagina a');
-
-            const urlParams = new URLSearchParams(window.location.search);
-            let paginaActual = parseInt(urlParams.get('pagina')) || 1;
-
-            if (anterior) {
-                anterior.addEventListener('click', () => {
-                    if (paginaActual > 1) {
-                        cambiarPagina(paginaActual - 1);
-                    }
-                });
-            }
-
-            if (siguiente) {
-                siguiente.addEventListener('click', () => {
-                    cambiarPagina(paginaActual + 1);
-                });
-            }
-
-            paginas.forEach(pagina => {
-                pagina.addEventListener('click', (event) => {
-                    event.preventDefault();
-                    const nuevaPagina = parseInt(event.target.textContent);
-                    cambiarPagina(nuevaPagina);
-                });
-            });
-        }
-
-        generarProductos();
+        // Cargar datos desde el archivo JSON
+        fetch('/data/products.json')
+            .then(response => response.json())
+            .then(data => setProductosData(data.products))
+            .catch(error => console.error('Error al cargar los datos:', error));
     }, []);
+
+    const sortProductos = (productos, criteria, direction) => {
+        if (criteria === 'Ninguno') return productos;
+
+        const sortedProductos = [...productos];
+        sortedProductos.sort((a, b) => {
+            if (a[criteria] < b[criteria]) return direction === 'asc' ? -1 : 1;
+            if (a[criteria] > b[criteria]) return direction === 'asc' ? 1 : -1;
+            return 0;
+        });
+        return sortedProductos;
+    };
+
+    const generarProductos = (page, sortedProductos) => {
+        const productosContainer = document.getElementById('productos');
+        productosContainer.innerHTML = ''; // Limpiar contenedor
+
+        const startIndex = (page - 1) * productsPerPage;
+        const endIndex = startIndex + productsPerPage;
+        const currentProducts = sortedProductos.slice(startIndex, endIndex);
+
+        currentProducts.forEach(producto => {
+            const containerDiv = document.createElement('div');
+            containerDiv.classList.add('productos-container2');
+
+            const imagenElement = document.createElement('img');
+            imagenElement.src = producto.image;
+            imagenElement.alt = producto.name;
+            containerDiv.appendChild(imagenElement);
+
+            const productoDetalleDiv = document.createElement('div');
+            productoDetalleDiv.classList.add('producto-detalle');
+
+            const tituloP = document.createElement('p');
+            tituloP.classList.add('producto-titulo');
+            tituloP.textContent = producto.name;
+
+            const fabricanteP = document.createElement('p');
+            fabricanteP.classList.add('producto-fabri');
+            fabricanteP.textContent = `Por: ${producto.brand}`;
+
+            const precioP = document.createElement('p');
+            precioP.classList.add('producto-precio');
+            precioP.textContent = `S/ ${producto.price.toFixed(2)}`;
+
+            productoDetalleDiv.appendChild(tituloP);
+            productoDetalleDiv.appendChild(fabricanteP);
+            productoDetalleDiv.appendChild(precioP);
+
+            containerDiv.appendChild(productoDetalleDiv);
+
+            containerDiv.addEventListener('click', () => {
+                const url = `./Detalle?titulo=${encodeURIComponent(producto.name)}&fabricante=${encodeURIComponent(producto.brand)}&imagenSrc=${encodeURIComponent(producto.image)}&precio=${encodeURIComponent(producto.price.toFixed(2))}&description=${encodeURIComponent(producto.description)}&features=${encodeURIComponent(producto.features.join(', '))}`;
+                window.location.href = url;
+            });
+
+            productosContainer.appendChild(containerDiv);
+        });
+    };
+
+    useEffect(() => {
+        if (productosData.length > 0) {
+            const sortedProductos = sortProductos(productosData, sortCriteria === 'Ninguno' ? null : sortCriteria.toLowerCase(), sortDirection);
+            generarProductos(currentPage, sortedProductos);
+        }
+    }, [productosData, currentPage, sortCriteria, sortDirection]);
+
+    const handleSortChange = (e) => {
+        const newSortCriteria = e.target.value;
+        if (sortCriteria === newSortCriteria) {
+            setSortDirection(prevDirection => prevDirection === 'asc' ? 'desc' : 'asc');
+        } else {
+            setSortCriteria(newSortCriteria);
+            setSortDirection('asc');
+        }
+        setCurrentPage(1); // Reset to first page on sort change
+    };
+
+    const totalPages = Math.ceil(productosData.length / productsPerPage);
 
     return (
         <>
@@ -129,29 +104,29 @@ function Busqueda() {
                 <div className="ordenar-container">
                     <div className="ordenar">
                         <span>Ordenar Por: </span>
-                        <select name="Precio">
-                            <option>Precio</option>
-                            <option>Nombre</option>
-                            <option>Opcion C</option>
+                        <select name="sort" onChange={handleSortChange} value={sortCriteria}>
+                            <option value="Ninguno">Ninguno</option>
+                            <option value="price">Precio</option>
+                            <option value="name">Nombre</option>
+                            <option value="brand">Fabricante</option>
                         </select>
                     </div>
                 </div>
                 <div className="resultados">
                     <div className="tituresul">
-                        <span>Resultados de Busqueda</span>
+                        <span>Resultados de Búsqueda</span>
                     </div>
                     <div id="productos"></div>
                 </div>
                 <div id="paginacion" className="paginacion">
                     <ul className="navega">
-                        <li className="anterior">« Anterior</li>
-                        <li className="pagina"><a href="#">1</a></li>
-                        <li className="pagina"><a href="#">2</a></li>
-                        <li className="pagina"><a href="#">3</a></li>
-                        <li className="pagina"><a href="#">4</a></li>
-                        <li className="puntos">...</li>
-                        <li className="pagina"><a href="#">11</a></li>
-                        <li className="siguiente">Siguiente »</li>
+                        <li className={`anterior ${currentPage === 1 ? 'disabled' : ''}`} onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}>« Anterior</li>
+                        {[...Array(totalPages)].map((_, i) => (
+                            <li key={i} className={`pagina ${currentPage === i + 1 ? 'active' : ''}`}>
+                                <a href="#" onClick={() => setCurrentPage(i + 1)}>{i + 1}</a>
+                            </li>
+                        ))}
+                        <li className={`siguiente ${currentPage === totalPages ? 'disabled' : ''}`} onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}>Siguiente »</li>
                     </ul>
                 </div>
             </div>
